24ee02bae43f4f3783092f8130cacd8e1fada12f
Michael Alexander <michael_alexander_20@outlook.com>
2019-09-12 13:15:03 +0100

    2 MVP targets down BOOOOM

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h1>Beerboys Never say Die!!!</h1>
+    <beer-detail :beer='selectedBeer'></beer-detail>
     <beers-list :beers='beers'></beers-list>
   </div>
 </template>
@@ -8,6 +9,8 @@
 <script>
 import ListComponent from './components/ListComponent.vue';
 import BeersList from './components/BeersList.vue';
+import BeerDetail from './components/BeerDetail.vue';
+import {eventBus} from './main.js'
 
 export default {
   name: 'app',
@@ -21,10 +24,15 @@ export default {
     fetch('https://api.punkapi.com/v2/beers')
     .then(res => res.json())
     .then(beers => this.beers = beers)
+
+    eventBus.$on('beer-selected', (beer) => {
+      this.selectedBeer = beer
+    })
   },
 
   components: {
-    "beers-list": BeersList
+    "beers-list": BeersList,
+    "beer-detail": BeerDetail
   }
 
 }
--- src/components/BeerDetail.vue
+++ src/components/BeerDetail.vue
@@ -1,9 +1,16 @@
 <template lang="html">
-
+  <article v-if="beer">
+    <p>Name: {{ beer.name }}</p>
+    <p>Tagline: {{ beer.tagline }}</p>
+    <p>A wee description: {{ beer.description }}</p>
+    <p>ABV: {{ beer.abv }}</p>
+  </article>
 </template>
 
 <script>
 export default {
+  name: 'beer-detail',
+  props: ['beer']
 }
 </script>
 
